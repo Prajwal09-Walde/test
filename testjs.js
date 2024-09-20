94eb3884c1aa245f1e98b8810c1849b648ed1c83
Prajwal09-Walde <pbwalde98@gmail.com>
2024-09-20 18:27:30 +0530

    First Commit

---
+++ testjs.js
@@ -0,0 +1,22 @@
+const text = document.getElementsByClassName(".heading"); 
+console.log(text);
+
+const button = document.getElementById("button");
+
+button.addEventListener('click', function() {
+    alert('Hello Prajwal !')
+})
+
+
+const a = 4;
+const b = 5;
+function addition() {
+    return a + b;
+}
+console.log(a+b);
+console.log(a*b);
+console.log(a/b);
+console.log(a%b);
+console.log(Math.pow(4, 5));
+
+
